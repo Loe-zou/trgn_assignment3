632b23ee46ad4a3a2d48fbf34ac065e2bb873ca3
Loe-zou <112156224+Loe-zou@users.noreply.github.com>
2022-09-23 17:11:05 -0700

    Update histogram.py

--- scripts/histogram.py
+++ scripts/histogram.py
@@ -9,11 +9,12 @@ if sys.argv[:1] == "-f":
 
 
 data = pd.read_csv(sys.argv[2], sep='\t', header=0)
-# data = pd.read_csv('expres.anal.csv', sep='\t', header=0)
+# data = pd.read_csv('expres.anal.csv', sep='\t', header=0) ##this works on jupyternotebook and prints but not working on server
 data.head()
 df = pd.DataFrame(data)
 cols = [1,2,3,4]
 df = df[int(df.columns[n])]
 ax = df.plot.hist()                     
 
+plt.savefig("histogram.png")
                         
