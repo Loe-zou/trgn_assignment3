52406af93dd24647423a502c71c36b04e8ce566a
Loe-zou <112156224+Loe-zou@users.noreply.github.com>
2022-10-31 22:47:03 -0700

    Makeup Commit

--- scripts/histogram.py
+++ scripts/histogram.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
-
 import pandas as pd
 import sys
 from matplotlib import pyplot as plt
 
-column = 2 
+
 
 if sys.argv[:1] == "-f":
     n = sys.argv[1]
     column = int(n[2])
+else:
+    column = 2
 
+    
+#check point
+#print(column)
 
 data = pd.read_csv(sys.argv[2], sep='\t', header=0)
-# data = pd.read_csv('expres.anal.csv', sep='\t', header=0) ##this works on jupyternotebook and prints but not working on server
-data.head()
 
+#data.head(1)
 
 # df = pd.DataFrame(data)
 # cols = [1,2,3,4]
@@ -24,6 +27,6 @@ col = data.columns[column]
 ax = data.loc[:,col]
 
 
-plot = df.plot.hist()                     
+plot = ax.plot.hist()                     
 
 plt.savefig("histogram.png")
